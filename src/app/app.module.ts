b0bd7648d052a3a5d730e088c530d387e36a18ae
Namitha Krishna <namithakrshn@gmail.com>
2020-04-07 14:57:19 +0000

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,96 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import {
+  MsalModule,
+  MsalInterceptor,
+  MSAL_CONFIG,
+  MSAL_CONFIG_ANGULAR,
+  MsalService,
+  MsalAngularConfiguration
+} from '@azure/msal-angular';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { Configuration } from 'msal';
+
+export const protectedResourceMap: [string, string[]][] = [
+  ['https://oidcfun.com/api', ['https://msapiuat.onmicrosoft.com/hello/demo.read']]
+];
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+function MSALConfigFactory(): Configuration {
+  return {
+    auth: {
+      clientId: '95e3fbf7-efbe-407e-aa4a-248604ec7027',
+      authority: "https://msapiuat.b2clogin.com/tfp/msapiuat.onmicrosoft.com/B2C_1_ISGDemoApplicationSignin",
+      validateAuthority: false,
+      redirectUri: "http://localhost.ms.com:4200/",
+      postLogoutRedirectUri: "http://localhost.ms.com:4200/",
+      navigateToLoginRequestUrl: true,
+    },
+    cache: {
+      cacheLocation: "localStorage",
+      storeAuthStateInCookie: isIE, // set to true for IE 11
+    },
+  };
+}
+
+function MSALAngularConfigFactory(): MsalAngularConfiguration {
+  return {
+    
+    consentScopes: [
+      "openid",
+      "https://msapiuat.onmicrosoft.com/hello/demo.read"
+    ],
+    unprotectedResources: ["https://www.microsoft.com/en-us/"],
+    protectedResourceMap,
+    extraQueryParameters: {}
+  };
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    AppRoutingModule,
+    MsalModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_CONFIG,
+      useFactory: MSALConfigFactory
+    },
+    {
+      provide: MSAL_CONFIG_ANGULAR,
+      useFactory: MSALAngularConfigFactory
+    },
+    MsalService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
